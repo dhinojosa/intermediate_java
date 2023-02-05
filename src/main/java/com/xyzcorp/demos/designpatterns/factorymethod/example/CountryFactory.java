6a162e59b9f25c5b9ac60fb918d9db49edd301ce
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2023-02-05 10:39:16 -0700

    removed unnecessary import

--- src/main/java/com/xyzcorp/demos/designpatterns/factorymethod/example/CountryFactory.java
+++ src/main/java/com/xyzcorp/demos/designpatterns/factorymethod/example/CountryFactory.java
@@ -1,7 +1,5 @@
 package com.xyzcorp.demos.designpatterns.factorymethod.example;
 
-import com.xyzcorp.javapatterns.chainresponsibility.Car;
-
 public interface CountryFactory {
     public Country createCountry(String name, byte[] flag, String capital);
 }
