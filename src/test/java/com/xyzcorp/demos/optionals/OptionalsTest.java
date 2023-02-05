8109e207795c57a6716cb86faa6508f765e173bd
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2023-02-05 10:35:56 -0700

    added more on Optionals test

--- src/test/java/com/xyzcorp/demos/optionals/OptionalsTest.java
+++ src/test/java/com/xyzcorp/demos/optionals/OptionalsTest.java
@@ -1,9 +1,13 @@
 package com.xyzcorp.demos.optionals;
 
+import com.xyzcorp.demos.collections.CollectionsTest;
+import com.xyzcorp.demos.generics.people.Person;
 import org.junit.Test;
 
 import java.util.NoSuchElementException;
 import java.util.Optional;
+import java.util.PriorityQueue;
+import java.util.Queue;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -57,6 +61,16 @@ public class OptionalsTest {
         Optional<Long> optionalLong = Optional.of(40L);
         Long result = optionalLong.orElseGet(this::getDefaultAverage);
         assertThat(result).isEqualTo(30L);
+    }
+
+    @Test
+    public void testPriorityQueue() throws Exception {
+        Queue<Person> queue = new PriorityQueue<>(new CollectionsTest.PersonComparator());
+        queue.offer(new Person("Franz", "Kafka"));
+        queue.offer(new Person("Jane", "Austen"));
+        queue.offer(new Person("Leo", "Tolstoy"));
+        queue.offer(new Person("Lewis", "Carroll"));
+        assert(Optional.ofNullable(queue.peek()).map(Person::getLastName).orElse("").equals("Austen"));
     }
 
     private Long getDefaultAverage() {
