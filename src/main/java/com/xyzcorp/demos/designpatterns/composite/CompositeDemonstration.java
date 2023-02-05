343a48c0a6dc35cf331bf1b9efc2b038309e9513
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2023-02-05 10:38:41 -0700

    added new information on composite java

--- src/main/java/com/xyzcorp/demos/designpatterns/composite/CompositeDemonstration.java
+++ src/main/java/com/xyzcorp/demos/designpatterns/composite/CompositeDemonstration.java
@@ -10,9 +10,9 @@ public class CompositeDemonstration {
         // (4 + 6) * (1 / 2)
         ArithmeticExpression calculation =
                 new Operand(
-                        new Operand(new java.lang.Number(4), Operation.PLUS, new java.lang.Number(6)),
+                        new Operand(new Number(4), Operation.PLUS, new Number(6)),
                         Operation.MULTIPLICATION,
-                        new Operand(new java.lang.Number(1), Operation.DIVISION, new java.lang.Number(2)));
+                        new Operand(new Number(1), Operation.DIVISION, new Number(2)));
 
         System.out.println(calculation.toString() + " = " + calculation.calculate());
     }
