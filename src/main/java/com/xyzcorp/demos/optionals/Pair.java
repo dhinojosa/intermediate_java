b1dbaf30c35a063331a556c89cdd978ad75d1082
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2023-03-07 09:25:23 -0700

    Created a Pair that we can use. Muahahahahah!

---
+++ src/main/java/com/xyzcorp/demos/optionals/Pair.java
@@ -0,0 +1,43 @@
+package com.xyzcorp.demos.optionals;
+
+import java.util.Objects;
+import java.util.StringJoiner;
+
+public class Pair<A, B> {
+    private final A a;
+    private final B b;
+
+    public Pair(A a, B b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    public A getA() {
+        return a;
+    }
+
+    public B getB() {
+        return b;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Pair<?, ?> pair = (Pair<?, ?>) o;
+        return Objects.equals(a, pair.a) && Objects.equals(b, pair.b);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(a, b);
+    }
+
+    @Override
+    public String toString() {
+        return new StringJoiner(", ", Pair.class.getSimpleName() + "[", "]")
+            .add("a=" + a)
+            .add("b=" + b)
+            .toString();
+    }
+}
